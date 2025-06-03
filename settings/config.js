f0b300210ad0529e4594d9c545b748495cc9ba7d
Andresv27728 <andrescamilovallejogomez701@gmail.com>
2025-06-03 09:41:41 -0500

    Add files via upload

---
+++ settings/config.js
@@ -0,0 +1,5 @@
+global.prefa = ['','!','.',',','🐤','🗿']
+global.owner = ['6285754675168']
+global.gambar = "https://files.catbox.moe/9wibx1.jpg"
+// GUNAKAN BOT DENGAN BIJAK !!!
+// CREATE BY VINN-XD
